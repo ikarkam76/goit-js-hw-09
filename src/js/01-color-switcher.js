16a03d2d044a2bd93eeec3662a6441c32b75cb92
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-22 11:28:26 +0300

    finished color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+startBtn.addEventListener('click', onChangeBgc)
+
+stopBtn.addEventListener('click', onStopChangeBgc);
+
+function onChangeBgc() {
+    timerId = setInterval(getRandomHexColor, 1000);
+    startBtn.setAttribute('disabled', 'disabled');
+}
+
+function onStopChangeBgc() {
+    clearInterval(timerId);
+    startBtn.removeAttribute('disabled', 'disabled');
+}
+
+function getRandomHexColor() {
+  return (document.body.style.backgroundColor = `#${Math.floor(
+    Math.random() * 16777215
+  ).toString(16)}`);
+};
