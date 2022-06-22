15c15cfba4221c40231d84035bfb01ea55fcffc9
ikarkam76 <104020905+ikarkam76@users.noreply.github.com>
2022-06-22 09:19:40 +0000

    Deploying to gh-pages from @ ikarkam76/goit-js-hw-09@ca9ef79d6e7b2477fa4bc77b565055c520a1ff88 🚀

---
+++ 02-timer.fd8356a9.js
@@ -0,0 +1,2 @@
+document.querySelector("#datetime-picker"),document.querySelector("[data-start]");const e=document.querySelector(".timer"),t=document.querySelectorAll(".field");e.style.display="flex",t.forEach((e=>{e.style.margin="10px",e.style.display="flex",e.style.flexDirection="column",e.style.color="blue",e.firstElementChild.style.textAlign="center",e.firstElementChild.style.fontSize="30px"}));
+//# sourceMappingURL=02-timer.fd8356a9.js.map
