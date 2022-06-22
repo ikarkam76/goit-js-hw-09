749cf7c9e8fbd02ebb67ffa4d995cfbd4660a999
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-22 13:16:05 +0300

    set flatpicker on timeinput

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,18 +1,33 @@
-
-
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
 const timeInput = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
 const timerBlock = document.querySelector('.timer');
 const timerField = document.querySelectorAll('.field');
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  dateFormat: 'Y-m-d H:i',
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
 
 timerBlock.style.display = 'flex';
 timerField.forEach(elem => {
-    elem.style.margin = '10px';
-    elem.style.display = 'flex';
-    elem.style.flexDirection = 'column';
-    elem.style.color = 'blue';
-    elem.firstElementChild.style.textAlign = 'center';
-    elem.firstElementChild.style.fontSize = '30px';
+  elem.style.margin = '10px';
+  elem.style.display = 'flex';
+  elem.style.flexDirection = 'column';
+  elem.style.color = 'blue';
+  elem.firstElementChild.style.textAlign = 'center';
+  elem.firstElementChild.style.fontSize = '30px';
 });
+
+timeInput.addEventListener(
+  'input',
+  flatpickr(timeInput, options)
+);
 
