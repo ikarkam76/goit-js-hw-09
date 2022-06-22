503646e93cd1498f8a889f21b268d62227add946
ikarkam76 <104020905+ikarkam76@users.noreply.github.com>
2022-06-22 10:16:36 +0000

    Deploying to gh-pages from @ ikarkam76/goit-js-hw-09@749cf7c9e8fbd02ebb67ffa4d995cfbd4660a999 🚀

--- 02-timer.db9fea10.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("#datetime-picker"),document.querySelector("[data-start]");var e=document.querySelector(".timer"),t=document.querySelectorAll(".field");e.style.display="flex",t.forEach((function(e){e.style.margin="10px",e.style.display="flex",e.style.flexDirection="column",e.style.color="blue",e.firstElementChild.style.textAlign="center",e.firstElementChild.style.fontSize="30px"}))}();
-//# sourceMappingURL=02-timer.db9fea10.js.map
