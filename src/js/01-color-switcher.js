cd17829e81bf6cd84e105ba8e620c4024eb126f5
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-23 12:08:43 +0300

    corrected collor-switcher buttons

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,18 +2,22 @@ const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
 let timerId = null;
 
+stopBtn.setAttribute('disabled', 'disabled');
+
 startBtn.addEventListener('click', onChangeBgc)
 
 stopBtn.addEventListener('click', onStopChangeBgc);
 
 function onChangeBgc() {
     timerId = setInterval(getRandomHexColor, 1000);
-    startBtn.setAttribute('disabled', 'disabled');
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled', 'disabled');
 }
 
 function onStopChangeBgc() {
     clearInterval(timerId);
     startBtn.removeAttribute('disabled', 'disabled');
+    stopBtn.setAttribute('disabled', 'disabled');
 }
 
 function getRandomHexColor() {
