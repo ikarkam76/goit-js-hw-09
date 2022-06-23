cc0c435e8ff046a5e11e907b336d970c8b2c2a85
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-06-23 11:47:07 +0300

    finished promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formInput = document.querySelector('.form');
+
+
+formInput.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formData = {};
+  new FormData(event.currentTarget).forEach((val, name) => {
+    formData[name] = val;
+  })
+  console.log(formData);
+  let delay = Number(formData.delay) - Number(formData.step);
+  for (let i = 1; i <= formData.amount; i += 1){
+    delay += Number(formData.step);
+    console.log(`i= ${i}, delay= ${delay}`)
+
+    createPromise(i, delay)
+      .then((message) => {
+       Notify.success(message);
+      })
+      .catch((message) => {
+        Notify.failure(message);
+      })
   }
+
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
